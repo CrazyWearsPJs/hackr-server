48a994644cd301d62560efcfd82339dc93695a49
Alex Gonzales <agonz056@ucr.edu>
2014-08-10 18:33:46 -0500

    added mongodb

--- server.go
+++ server.go
@@ -2,14 +2,28 @@ package main
 
 import (
 	"github.com/go-martini/martini"
-	//"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2"
     //"gopkg.in/mgo.v2/bson"
-    //"os"
-    //"fmt"
+    "os"
+    "fmt"
 )
 
 func main() {
-	m := martini.Classic()
+    uri:= os.Getenv("MONGOHQ_URL")
+    if uri == "" {
+        fmt.Println("no connection string provided")
+        os.Exit(1)
+    }
+
+    sess, err := mgo.Dial(uri)
+    if err != nil {
+        fmt.Printf("Can't connect to mongo, got error %v\n", err)
+        os.Exit(1)
+    }
+
+    defer sess.Close()
+
+    m := martini.Classic()
     m.Get("/", helloHandler)
 	m.Run()
 }
@@ -17,18 +31,3 @@ func main() {
 func helloHandler() (int, string) {
 	return 200, "Hello World"
 }
-/*
-func connect() {
-    if uri == ""{
-        fmt.Println("no connection string provided")
-        os.Exit(1)
-    }
-
-    sess, err := mgo.Dial(uri)
-    if err != nil {
-        fmt.Printf("Can't connect to mongo, go err %v\n", err)
-        os.Exit(1)
-    }
-    defer sess.Close()
-}
-*/
