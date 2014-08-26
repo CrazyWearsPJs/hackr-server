8a01e6b03832a2a4c456695db19298a9dba49225
Alex Gonzales <agonz056@ucr.edu>
2014-08-25 23:54:36 -0500

    added submission handler

--- handlers.go
+++ handlers.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	 "fmt"
+	"encoding/json"
+	"fmt"
 	_ "github.com/codegangsta/negroni"
 	_ "github.com/garyburd/redigo/redis"
 	_ "gopkg.in/mgo.v2"
 	_ "gopkg.in/mgo.v2/bson"
+	"log"
 	"net/http"
 	_ "net/url"
 	_ "os"
@@ -14,14 +16,55 @@ import (
 func SetupMux() *http.ServeMux {
 	server_mux := http.NewServeMux()
 	server_mux.HandleFunc("/", IndexHandler)
-    server_mux.HandleFunc("/login", LoginHandler)
+	server_mux.HandleFunc("/login", LoginHandler)
+	server_mux.HandleFunc("/api/v1/user/exercises", SubmissionHandler)
 	return server_mux
 }
 
-func IndexHandler(res http.ResponseWriter, req *http.Request){
-    fmt.Fprintf(res, "Hello There")
+func IndexHandler(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(res, "Hello There")
 }
 
 func LoginHandler(res http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(res, "hi")
+}
+
+type submitResponse struct {
+	Status string `json::"status"`
+	Error  string `json::"error"`
+}
+
+type submitRequest struct {
+	Email string `json::"email"`
+	Key   string `json::"key"`
+	Code  string `json::"code"`
+}
+
+func SubmissionHandler(res http.ResponseWriter, req *http.Request) {
+	var sreq *submitRequest
+
+	switch req.Method {
+	case "POST":
+		decoder := json.NewDecoder(req.Body)
+		if err := decoder.Decode(&sreq); err != nil {
+			log.Printf("Error parsing response: %v", err)
+			res.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		sres := submitResponse{Status: "OK", Error: "None"}
+
+		res.Header().Set("Access-Control-Allow-Origin", "*")
+		res.Header().Set("Access-Control-Allow-Headers", "X-Requested-With")
+		res.Header().Set("Content-Type", "application/json")
+
+		encoder := json.NewEncoder(res)
+		if err := encoder.Encode(sres); err != nil {
+			log.Printf("Error parsing response: %v", err)
+			res.WriteHeader(http.StatusInternalServerError)
+		}
+		res.WriteHeader(http.StatusCreated)
+	default:
+		res.WriteHeader(http.StatusBadRequest)
+	}
 }
